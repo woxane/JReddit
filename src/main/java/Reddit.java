5c183bc5b40a021bb22e54d64a164c42646934d9
woxane <mohsen.minavand1384@gmail.com>
2024-03-25 17:50:28 +0330

    Implement emailValidator function

--- src/main/java/Reddit.java
+++ src/main/java/Reddit.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
-
 public class Reddit {
     ArrayList<Subreddit> Subreddits = new ArrayList<>();
     ArrayList<Account> accounts = new ArrayList<>();
+
+    public static boolean emailValidator(String email) {
+        String regEx = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        return email.matches(regEx);
+    }
 }
