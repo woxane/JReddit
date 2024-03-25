149d0e6d8a1f74bc61d0d145bbb936b32e8bc3df
woxane <mohsen.minavand1384@gmail.com>
2024-03-25 16:53:59 +0330

    Implement Account constructor

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,19 @@
+import java.util.UUID;
+import org.apache.commons.codec.digest.DigestUtils;
+
 public class Account {
+    String name;
+    String username;
+    String password;
+    String emailAddress;
+    final UUID accountID;
+    int karma;
+
+    Account (String name , String username , String password , String emailAddress) {
+        this.name = name;
+        this.username = username;
+        this.password = DigestUtils.sha256Hex(password);
+        this.emailAddress = emailAddress;
+        this.accountID = UUID.randomUUID();
+    }
 }
