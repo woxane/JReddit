3beeb3c6ed4bf4fef72d057fdb7a256719b3b411
woxane <mohsen.minavand1384@gmail.com>
2024-03-25 18:00:01 +0330

    Implement getAllUsername function

--- src/main/java/Reddit.java
+++ src/main/java/Reddit.java
@@ -1,10 +1,22 @@
 import java.util.ArrayList;
 public class Reddit {
-    ArrayList<Subreddit> Subreddits = new ArrayList<>();
-    ArrayList<Account> accounts = new ArrayList<>();
+    static ArrayList<Subreddit> Subreddits = new ArrayList<>();
+    static ArrayList<Account> accounts = new ArrayList<>();
 
     public static boolean emailValidator(String email) {
         String regEx = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
         return email.matches(regEx);
     }
+
+
+    public static ArrayList<String> getAllUsernames() {
+        ArrayList<String> usernames = new ArrayList<>();
+
+        for (Account account : accounts) {
+            usernames.add(account.username);
+        }
+
+        return usernames;
+    }
+
 }
