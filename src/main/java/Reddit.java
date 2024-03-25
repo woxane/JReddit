bbfd6cbc2f4ba837e366eabeaee564fc74990832
woxane <mohsen.minavand1384@gmail.com>
2024-03-25 18:18:27 +0330

    Implement signUp function

--- src/main/java/Reddit.java
+++ src/main/java/Reddit.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Reddit {
     static ArrayList<Subreddit> Subreddits = new ArrayList<>();
     static ArrayList<Account> accounts = new ArrayList<>();
@@ -19,4 +21,47 @@ public class Reddit {
         return usernames;
     }
 
+    public void signUp() {
+        String name;
+        String username;
+        String password;
+        String emailAddress;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hey , welcome to JReddit sign up page .");
+        System.out.print("\tTo continue please enter your email address : ");
+
+        do {
+            emailAddress = scanner.nextLine();
+
+            if (emailValidator(emailAddress)) {
+                break;
+            } else {
+                System.out.print("Sorry but the email address you enter is not valid </3 . \n\tTry something else: ");
+            }
+
+        } while (true);
+
+        System.out.print("Good , now please enter your username : ");
+
+        do {
+           username = scanner.nextLine();
+
+           if (getAllUsernames().contains(username)) {
+               System.out.print("Sorry but the username you enter is already in use </3 . \n\tTry something else : ");
+           } else {
+               break;
+           }
+
+        } while(true);
+
+        System.out.print("Oh it's going well , please enter your password : ");
+        password = scanner.nextLine();
+
+        System.out.print("Just one more step .\n\tEnter your name : ");
+        name = scanner.nextLine();
+
+        Account account = new Account(name , username , password , emailAddress);
+        accounts.add(account);
+    }
 }
