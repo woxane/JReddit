c4c6ffd3a998c7543febca807bddae135594c7da
woxane <mohsen.minavand1384@gmail.com>
2024-03-25 17:02:23 +0330

    create Comment.java and implement comment constructor

---
+++ src/main/java/Comment.java
@@ -0,0 +1,10 @@
+public class Comment {
+    final String commentAuthor;
+    final String commentContent;
+
+
+    Comment (String username , String content) {
+        this.commentAuthor = username;
+        this.commentContent = content;
+    }
+}
