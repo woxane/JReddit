7a983aa0ae86109c0ff80b943ad2e7a945255c73
woxane <mohsen.minavand1384@gmail.com>
2024-03-25 17:10:25 +0330

    add vote variable

--- src/main/java/Comment.java
+++ src/main/java/Comment.java
@@ -1,10 +1,12 @@
 public class Comment {
     final String commentAuthor;
     final String commentContent;
+    int vote;
 
 
     Comment (String username , String content) {
         this.commentAuthor = username;
         this.commentContent = content;
+        this.vote = 0;
     }
 }
