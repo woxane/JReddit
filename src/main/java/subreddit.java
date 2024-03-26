fe2c98633be51bd846ff21b6815b53a7f11ff196
woxane <mohsen.minavand1384@gmail.com>
2024-03-26 15:56:43 +0330

    Add name variable to Subreddit class

--- src/main/java/subreddit.java
+++ src/main/java/subreddit.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 
 public class Subreddit {
+    String name;
     Account owner;
     ArrayList<Account> admins = new ArrayList<>();
     ArrayList<Account> banedUsers = new ArrayList<>();
     ArrayList<Account> users = new ArrayList<>();
     ArrayList<Post> posts = new ArrayList<>();
 
-    Subreddit (Account owner) {
+    Subreddit (Account owner , String name) {
         this.owner = owner;
+        this.name = name;
     }
 }
