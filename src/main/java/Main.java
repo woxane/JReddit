6303672c2bda9ff16a75f004f2bb48ea33ecc3c0
woxane <mohsen.minavand1384@gmail.com>
2024-03-26 15:48:50 +0330

    Implement authPage function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    }
+
+    public static Account authPage() {
+        int authMethod;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Hey welcome to JReddit , to continue choose one of the above options : \n\t1) Log in\t\t2) Sign up\n : ");
+
+        do {
+            authMethod = scanner.nextInt();
+
+            if (authMethod == 1 | authMethod == 2) {
+                Account account;
+                switch (authMethod) {
+                    case 1 :
+                        account = Reddit.authenticate();
+                        if (account == null) {
+                            System.out.println("One or more of the following credentials is incorrect : \n\tusername , email , or password");
+                            return null;
+                        } else {
+                            return account;
+
+                        }
+
+                    case 2 :
+                        account = Reddit.signUp();
+                        if (account == null) {
+                            System.out.println("PROBLEMMMMM !!!!!");
+
+                        } else {
+                            return account;
+                        }
+                }
+
+
+            } else {
+                System.out.print("Please choose between 1 or 2 : ");
+            }
+        } while (true);
+
     }
 }
