08630f9fd8c32b8ecbeb53495b556590978112f2
woxane <mohsen.minavand1384@gmail.com>
2024-03-26 16:13:06 +0330

    Implement viewPost function

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -8,11 +8,21 @@ public class Post {
     int vote;
     ArrayList<Comment> comments = new ArrayList<>();
 
-    Post (Account username , String title , String content , Subreddit subreddit) {
+    Post (Account username , String title, String content , Subreddit subreddit) {
         this.author = username;
         this.title = title;
         this.content = content;
         this.subreddit = subreddit;
         this.vote = 0;
+    }
+
+    public void viewPost() {
+        System.out.println("r/" + this.subreddit.name + " via u/" + this.author.username);
+        System.out.println(this.title);
+        System.out.println("\n" + this.content);
+
+        for (Comment comment : comments) {
+            comment.viewComment();
+        }
     }
 }
