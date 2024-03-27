54f03dab7b18bba2e9b282db8a4691b8be630330
woxane <mohsen.minavand1384@gmail.com>
2024-03-27 21:10:38 +0330

    Create Vote class and Implement Vote constructor

---
+++ src/main/java/Vote.java
@@ -0,0 +1,10 @@
+public class Vote {
+    Account voter;
+    boolean isUpVote;
+
+    Vote(Account voter , boolean isUpVote) {
+        this.voter = voter;
+        this.isUpVote = isUpVote;
+    }
+
+}
